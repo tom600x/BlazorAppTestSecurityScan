0bfef514126320ccf45c4c91812f96f74b34997c
Tom Ordille <thordill@microsoft.com>
2024-04-19 18:13:31 -0400

    test

--- BlazorApp.Client/Services/ClientWeatherService.cs
+++ BlazorApp.Client/Services/ClientWeatherService.cs
@@ -1,4 +1,5 @@
 using System.Net.Http.Json;
+using System.Text.RegularExpressions;
 using BlazorApp.Client.Models;
 
 namespace BlazorApp.Client.Services;
@@ -7,4 +8,21 @@ public class ClientWeatherService(HttpClient httpClient) : IWeatherService
 {
     public Task<WeatherForecast[]> GetWeather() =>
         httpClient.GetFromJsonAsync<WeatherForecast[]>("/api/weather")!;
+
+    public string UpdateCustomerPassword(string txtUsername, string txtPassword)
+    {
+        string userName = txtUsername;
+        string password = txtPassword;
+
+        Regex testPassword = new Regex(userName);
+        Match match = testPassword.Match(password);
+        if (match.Success)
+        {
+            return "Do not include name in password.";
+        }
+        else
+        {
+            return "Good password.";
+        }
+    }
 }
