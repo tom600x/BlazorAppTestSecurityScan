c02d7cab044d74c257f699da9debb222ef2c5493
Tom Ordille <thordill@microsoft.com>
2024-04-19 13:52:22 -0400

    test sec

--- BlazorApp/ServerWeatherService.cs
+++ BlazorApp/ServerWeatherService.cs
@@ -1,5 +1,7 @@
 using BlazorApp.Client.Models;
 using BlazorApp.Client.Services;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BlazorApp;
 
@@ -21,4 +23,23 @@ public class ServerWeatherService : IWeatherService
 
         return forecasts;
     }
+
+    public string UpdateCustomerPassword(string txtUsername, string txtPassword)
+    {
+        string userName = txtUsername;
+        string password = txtPassword;
+
+        Regex testPassword = new Regex(userName);
+        Match match = testPassword.Match(password);
+        if (match.Success)
+        {
+           return "Do not include name in password.";
+        }
+        else
+        {
+            return "Good password.";
+        }
+    }
+}
+
 }
